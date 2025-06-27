758e940f125d43499bdb70a06e3d931d2ff68c87
Divyanshu T <tdivyanshu028@gmail.com>
2025-06-27 10:47:06 -0700

    Update view_groups_screen1.dart

--- lib/screen/view_groups_screen1.dart
+++ lib/screen/view_groups_screen1.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../data/food_data.dart';
 import '../widgets/companion_card.dart';
-import '../model/companion_model.dart';
+import '../model/food_model.dart';
 import 'chat_food.dart';
 
 class ViewGroupsScreen extends StatefulWidget {
